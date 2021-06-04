11e49b7fa9eec728dbf604a465869f8433fdc8d4
Vaidehi21910961 <wagaj21910961@viit.ac.in>
2021-06-04 20:32:25 +0530

    Robofriends App

---
+++ src/robots.js
@@ -0,0 +1,64 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Kim Minsoek',
+		username: 'Xiumin',
+		email: 'xiumin@gmail.com'
+	},
+
+	{
+		id: 2,
+		name: 'Kim Junmyeon',
+		username: 'Suho',
+		email: 'suho@gmail.com'
+	},
+
+	{
+		id: 3,
+		name: 'Zhang Yixing',
+		username: 'Lay',
+		email: 'lay@gmail.com'
+	},
+
+	{
+		id: 4,
+		name: 'Byun Baekhyun',
+		username: 'Baekhyun',
+		email: 'baekhyun@gmail.com'
+	},
+
+	{
+		id: 5,
+		name: 'Kim Jongdae',
+		username: 'Chen',
+		email: 'chen@gmail.com'
+	},
+
+	{
+		id: 6,
+		name: 'Park Chanyeol',
+		username: 'Chanyeol',
+		email: 'chanyeol@gmail.com'
+	},
+
+	{
+		id: 7,
+		name: 'Do Kyungsoo',
+		username: 'D.O.',
+		email: 'd.o@gmail.com'
+	},
+
+	{
+		id: 8,
+		name: 'Kim Jongin',
+		username: 'Kai',
+		email: 'kai@gmail.com'
+	},
+
+	{
+		id: 9,
+		name: 'Oh Sehun',
+		username: 'Sehun',
+		email: 'sehun@gmail.com'
+	}
+]
